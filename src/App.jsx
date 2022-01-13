ef4cb088074a76ec134b6fa1d3090e3e562e39b3
sapdey <saptarshidey13@gmail.com>
2022-01-14 00:31:30 +0530

    domain route fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import ApplicationRoutes from "./routes";
 import "./App.css";
 const App = () => {
   return (
-    <BrowserRouter basename="/bhoomi-basera">
+    <BrowserRouter>
       <ApplicationRoutes />
     </BrowserRouter>
   );
